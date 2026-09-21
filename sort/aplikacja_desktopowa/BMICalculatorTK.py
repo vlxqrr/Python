024ffc375a94ae5d7b75b7b804b677e86d5b95bb
vlxqrr <kamilawydraa@gmail.com>
2026-09-21 09:36:44 +0200

    tk

---
+++ sort/aplikacja_desktopowa/BMICalculatorTK.py
@@ -0,0 +1,12 @@
+import tkinter as tk
+class Sort:
+     def __init__(self, root):
+        self.root = root
+    
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    root.title("Sort")
+    root.geometry("1920x1080")
+    app = Sort(root)
+    app.mainloop()
