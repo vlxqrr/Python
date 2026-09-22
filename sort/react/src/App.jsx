cf3f6f06b7a7f45b5082c5d6c03212dedda0629f
vlxqrr <kamilawydraa@gmail.com>
2026-09-22 12:14:22 +0200

    sort react

--- sort/react/src/App.jsx
+++ sort/react/src/App.jsx
@@ -1,18 +1,25 @@
 import { useState } from 'react'
 import './App.css'
 import { bubbleSort } from './assets/scripts/sort.js'
+import { selectionSort } from './assets/scripts/sort.js'
+import { insertionSort } from './assets/scripts/sort.js'
+import { mergeSort } from './assets/scripts/sort.js'
+import { heapSort } from './assets/scripts/sort.js'
 function App() {
   return (
     <div>
       <h2>Sorting</h2>
       <p>Tablica przed sortowaniem:</p>
-      <input type="text" id="userInput" />
-      <input type="button" value="BubbleSort" />
-      <input type="button" value="SelectionSort" />
-      <input type="button" value="InsertionSort" />
-      <input type="button" value="MergeSort" />
-      <input type="button" value="HeapSort" />
-      <p id="result"></p>
+      <form>
+        <input type="text" id="userInput" />
+        <input type="button" value="BubbleSort" onClick={bubbleSort} />
+        <input type="button" value="SelectionSort" onClick={selectionSort} />
+        <input type="button" value="InsertionSort" onClick={insertionSort} />
+        <input type="button" value="MergeSort" onClick={mergeSort} />
+        <input type="button" value="HeapSort" onClick={heapSort} />
+        <p id="result"></p>
+      </form>
+
 
 
     </div>
--- sort/react/src/assets/scripts/sort.js
+++ sort/react/src/assets/scripts/sort.js
@@ -1,6 +1,181 @@
 export function bubbleSort() {
-    const array = [];
-    userInput = document.getElementById("userInput").value;
-    array.trim().split().map(numStr => Number(numStr)).filter(num => !isNaN(num))
+    let userInput = document.getElementById("userInput").value;
+    const array = userInput
+        .trim()
+        .split(/[\s,]+/)
+        .map(numStr => Number(numStr))
+        .filter(num => !isNaN(num));
 
+    if (array.length === 0) {
+        document.getElementById("result").innerHTML = "Wprowadź poprawne liczby!";
+        return;
+    }
+
+    let n = array.length;
+    for (let i = 0; i < n - 1; i++) {
+        for (let j = 0; j < n - 1 - i; j++) {
+            if (array[j] > array[j + 1]) {
+                [array[j], array[j + 1]] = [array[j + 1], array[j]];
+            }
+        }
+    }
+    const result = array.join(", ");
+    document.getElementById("result").innerHTML = result;
+}
+
+export function selectionSort() {
+    let userInput = document.getElementById("userInput").value;
+    const array = userInput
+        .trim()
+        .split(/[\s,]+/)
+        .map(numStr => Number(numStr))
+        .filter(num => !isNaN(num));
+
+    if (array.length === 0) {
+        document.getElementById("result").innerHTML = "Wprowadź poprawne liczby!";
+        return;
+    }
+    let n = array.length;
+
+    for (let i = 0; i < n - 1; i++) {
+        let min = i;
+        for (let j = i + 1; j < n; j++) {
+            if (array[j] < array[min]) {
+                min = j;
+            }
+        }
+        if (min !== i) {
+            [array[i], array[min]] = [array[min], array[i]];
+        }
+    }
+    const result = array.join(", ");
+    document.getElementById("result").innerHTML = result;
+
+}
+
+export function insertionSort() {
+    let userInput = document.getElementById("userInput").value;
+    const array = userInput
+        .trim()
+        .split(/[\s,]+/)
+        .map(numStr => Number(numStr))
+        .filter(num => !isNaN(num));
+
+    if (array.length === 0) {
+        document.getElementById("result").innerHTML = "Wprowadź poprawne liczby!";
+        return;
+    }
+
+    let n = array.length;
+    for (let i = 1; i < n; i++) {
+        let key = array[i];
+        let j = i - 1;
+        while (j >= 0 && key < array[j]) {
+            array[j + 1] = array[j];
+            j -= 1;
+        }
+        array[j + 1] = key;
+    }
+    const result = array.join(", ");
+    document.getElementById("result").innerHTML = result;
+}
+
+export function mergeSort(array) {
+    let isInitial = false;
+
+    if (!Array.isArray(array)) {
+        isInitial = true;
+        let userInput = document.getElementById("userInput").value;
+        array = userInput
+            .trim()
+            .split(/[\s,]+/)
+            .map(numStr => Number(numStr))
+            .filter(num => !isNaN(num));
+        if (array.length === 0) {
+            document.getElementById("result").innerHTML = "Wprowadź poprawne liczby!";
+            return;
+        }
+    }
+
+    if (array.length > 1) {
+        let mid = Math.floor(array.length / 2);
+        let left = array.slice(0, mid);
+        let right = array.slice(mid);
+
+        mergeSort(left);
+        mergeSort(right);
+        let i = 0;
+        let j = 0;
+        let k = 0;
+        while (i < left.length && j < right.length) {
+            if (left[i] < right[j]) {
+                array[k] = left[i];
+                i += 1;
+            } else {
+                array[k] = right[j];
+                j += 1;
+            }
+            k += 1;
+        }
+
+        while (i < left.length) {
+            array[k] = left[i];
+            i += 1;
+            k += 1;
+        }
+
+        while (j < right.length) {
+            array[k] = right[j];
+            j += 1;
+            k += 1;
+        }
+    }
+    if (isInitial) {
+        const result = array.join(", ");
+        document.getElementById("result").innerHTML = result;
+    }
+}
+export function heapify(array, n, i) {
+    let largest = i;
+    let left = 2 * i + 1;
+    let right = 2 * i + 2;
+
+    if (left < n && array[left] > array[largest]) {
+        largest = left;
+    }
+    if (right < n && array[right] > array[largest]) {
+        largest = right;
+    }
+    if (largest !== i) {
+        [array[i], array[largest]] = [array[largest], array[i]];
+
+        heapify(array, n, largest);
+    }
+}
+export function heapSort() {
+    let userInput = document.getElementById("userInput").value;
+    const array = userInput
+        .trim()
+        .split(/[\s,]+/)
+        .map(numStr => Number(numStr))
+        .filter(num => !isNaN(num));
+
+    if (array.length === 0) {
+        document.getElementById("result").innerHTML = "Wprowadź poprawne liczby!";
+        return;
+    }
+    let n = array.length;
+
+    for (let i = Math.floor(n / 2) - 1; i >= 0; i--) {
+        heapify(array, n, i);
+    }
+
+
+    for (let i = n - 1; i > 0; i--) {
+        [array[0], array[i]] = [array[i], array[0]];
+        heapify(array, i, 0);
+    }
+
+    const result = array.join(", ");
+    document.getElementById("result").innerHTML = result;
 }
