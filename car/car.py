97e2e08cddd91e4a6d28e98cdde7564c9c9cda7a
vlxqrr <kamilawydraa@gmail.com>
2026-09-24 13:14:52 +0200

    fix: types feat: startEngine

--- car/car.py
+++ car/car.py
@@ -1,16 +1,34 @@
 class car:
-    def __init__(self, mark, model, caryear, fuel, capacity, speed, wear, engine_on):
-        self.mark = "" # jaka marka
-        self.model = "" # jaki model
-        self.caryear = "" # jaki rocznik
-        self.fuel = 0 # ile jest paliwa
-        self.capacity = "" # jaka jest pojemnosc
-        self.speed = 0 # jaka jest aktualna predkosc
-        self.wear = 0 # jakie jest zuzycie na 100km
-        self.engine_on = False # czy silnik jest wlaczony
+    def __init__(self, 
+                 mark: str = "", 
+                 model: str = "", 
+                 caryear: int = "", 
+                 fuel : float = 0.0, 
+                 capacity: str = "", 
+                 speed: float = 0.0, 
+                 wear: float = 0.0, 
+                 engine_on: bool = False,
+                 ):
+        
+        self.mark = mark # jaka marka
+        self.model = model # jaki model
+        self.caryear = caryear # jaki rocznik
+        self.fuel = fuel # ile jest paliwa
+        self.capacity = capacity # jaka jest pojemnosc
+        self.speed = speed # jaka jest aktualna predkosc
+        self.wear = wear # jakie jest zuzycie na 100km
+        self.engine_on = engine_on # czy silnik jest wlaczony
 
-    def turnOn(self):
-        self.engine_on = True
+    def car(self):
+        car = self.mark
+
+    def startEngine(self):
+        if self.fuel > 0:
+            self.engine_on = True
+            print(f"Auto {self.mark} wlaczone!")
+        else:
+            print("Nie ma paliwa")
+
 
     def showState(self):
         print("Status:")
@@ -21,7 +39,7 @@ class car:
         print(f"Pojemnosc auta {self.capacity}")
         print(f"Aktualna predkosc {self.speed}")
         print(f"Zuzycie na 100km {self.wear}")
-        print(f"Czy silnik jest wlaczony {self.turnOn()}")
+        print(f"Czy silnik jest wlaczony {self.engine_on}")
         
     def speedUp(self):
         pass
@@ -35,25 +53,22 @@ class car:
     def run(self):
         self.mark = input("Podaj marke auta: ")
         self.model = input("Podaj model auta: ")
-        self.caryear = int(input("Podaj rok wyprodukowania: "))
-        self.fuel = 0
-        self.capacity = int(input("Podaj pojemnosc: "))
-        self.speed = 0
-        self.engine_on = False
+        self.caryear = float(input("Podaj rok wyprodukowania: "))
+        self.fuel = float(input("Podaj ile jest paliwa"))
+        self.capacity = float(input("Podaj pojemnosc: "))
 
         print("Wybierz akcje ktora chcesz zrobic\n"
         "1) Uruchom silnik\n"
         "2) Sprawdz stan pojazdu\n"
         "3) Przyspiesz\n"
         "4) Hamuj\n"
-        "5) Tankuj\n" \
+        "5) Tankuj\n" 
         "6) Zakoncz")
 
         while True:
             self.action = input("Podaj ktora akcje chcesz wykonac: ")
             if self.action == "1":
-                self.turnOn()
-                print("Wlaczono silnik")
+                self.startEngine()
             elif self.action == "2":
                 self.showState()
             elif self.action == "3":
